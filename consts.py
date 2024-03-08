da0edf780f2dfa912228adf86a4f66e89ae7a887
PeacefulPythonist <160988227+PeacefulPythonist@users.noreply.github.com>
2024-03-08 08:47:18 +0500

    docs

--- consts.py
+++ consts.py
@@ -1,3 +1,13 @@
+"""
+consts.py: Constants and configuration settings for the project.
+
+Variables:
+- TEST_MODE (bool): Flag indicating whether the project is in test mode.
+- MAIN_BOT (str): Telegram bot token (used in AWS Lambda).
+- BOT_URL (str): Telegram bot URL for API requests.
+- UPDATE_URL (str): URL for getting updates from Telegram.
+- SEND_MESSAGE (str): URL for sending messages via Telegram.
+"""
 
 from api_keys import MAIN_BOT, TEST_BOT, ADMIN_ID
 
